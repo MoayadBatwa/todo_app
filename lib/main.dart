3a896a84fac3ed8c9ec9292d3913b5f79685bcb8
Moayad Batwa <123902670+MoayadBatwa@users.noreply.github.com>
2026-02-16 12:05:45 +0300

    ListTail and I awnt to add checkbox tp it

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+
+        appBar: AppBar(),
+
+        body: 
+        ListTile(
+          leading: Icon(Icons.task),
+
+          title: Text("Task 1"),
+
+          subtitle: Text("Task Description"),
+
+          
+
         ),
       ),
     );
