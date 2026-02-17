807bb456fa1accc3edd99bac82568622f68570db
Moayad Batwa <123902670+MoayadBatwa@users.noreply.github.com>
2026-02-17 15:17:35 +0300

    list of images with the tasks using netwrok

--- lib/Screens/task_screen.dart
+++ lib/Screens/task_screen.dart
@@ -11,7 +11,13 @@ class TaskScreen extends StatefulWidget {
 }
 
 class _TaskScreenState extends State<TaskScreen> {
-  
+  final List<String> imagesPath = [
+    "https://thumbs.dreamstime.com/b/top-rating-banner-sale-label-business-concept-logo-template-vector-illustration-stock-image-top-rating-banner-sale-label-business-261390195.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_oj3uHUdjvCsDrcvzT4sThOaTpg0vosrf6w&s", 
+    "https://media.istockphoto.com/id/1294684226/vector/top-3-red-button-flat-vector-illustration-on-white-background.jpg?s=612x612&w=0&k=20&c=xEWpyR7PJq6lrzsTAhC5EEjmI-yqQ9rARRIw3dv6bqg=",
+    "https://img.freepik.com/premium-vector/number-four-top-star-badge-vector-rating-symbol-bright-ribbon-design_213497-3682.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyynPkq3g_Xq-hhURfQJSdmem7qhFR1xRb3w&s",
+    ];
 
   @override
   Widget build(BuildContext context) {
@@ -33,42 +39,47 @@ class _TaskScreenState extends State<TaskScreen> {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                SizedBox(
-                  // height: 200,
-                  width: 150,
-                  
-                  child:
-                    ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(25)),
-                      child:
-                        Image.asset("images/1top.png"),
-                    )
-                ),
-
-                SizedBox(height: 10),
-                
                 
                 ...List.generate(DataSource().allTasksLength(), (index) {
-                  return 
-                    Card(
-                      color: const Color.fromARGB(214, 227, 229, 230),
-                      child: ListTile( 
-                        leading: Icon(Icons.task),
+                  return
+                    Column(children: [
+                      SizedBox(height: 10),
+
+                      SizedBox(
+                      // height: 200,
+                      width: 150,
                       
-                        title: Text(DataSource().allTasks[index].titleTask),
-                      
-                        subtitle: Text(DataSource().allTasks[index].descriptionTask),
-                      
-                        trailing: Checkbox(value: DataSource().allTasks[index].completed, onChanged: (value){
-                          // DataSource().allTasks[index].completed = !DataSource().allTasks[index].completed;
-                          DataSource().switchCheckedByID(DataSource().allTasks[index].id);
-                          setState(() {});
-                        }),
-                      
-                        
-                      
+                      child:
+                        ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(25)),
+                          child:
+                            Image.network(imagesPath[index]),
+                        )
                       ),
-                    );
+
+                      SizedBox(height: 10),
+
+                      Card(
+                        color: const Color.fromARGB(214, 227, 229, 230),
+                        child: ListTile( 
+                          leading: Icon(Icons.task),
+                        
+                          title: Text(DataSource().allTasks[index].titleTask),
+                        
+                          subtitle: Text(DataSource().allTasks[index].descriptionTask),
+                        
+                          trailing: Checkbox(value: DataSource().allTasks[index].completed, onChanged: (value){
+                            // DataSource().allTasks[index].completed = !DataSource().allTasks[index].completed;
+                            DataSource().switchCheckedByID(DataSource().allTasks[index].id);
+                            setState(() {});
+                          }),
+                        
+                          
+                        
+                        ),
+                      )
+                    ],);
+                    
                   },
                 ),
 
